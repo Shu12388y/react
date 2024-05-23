8704087eca559e22f8c3311268f2c318f82d2b0e
Shu12388y <spl.sp999@gmail.com>
2024-05-23 20:20:15 +0530

    file added

---
+++ basic/src/components/Render.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useState } from "react";
+
+const Render = ()=>{
+    const[data,setData]  = useState(false);
+
+    if(data){
+        return(
+            <>
+            <h1>Hello This is shubham</h1>
+            <button onClick={()=>setData(false)}>Click me</button>
+            </>
+        )
+    }
+    else{
+        return(
+            <>
+            <h1>This is unknown</h1>
+            <button onClick={()=>setData(true)}>Click me</button>
+            
+            </>
+        )
+    }
+    
+} 
+
+
+export default Render
