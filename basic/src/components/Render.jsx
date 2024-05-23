8c2581992751af37cde7d5c6198e230246f9cad3
Shu12388y <spl.sp999@gmail.com>
2024-05-23 20:25:52 +0530

    file addeD

--- basic/src/components/Render.jsx
+++ basic/src/components/Render.jsx
@@ -4,23 +4,26 @@ import { useState } from "react";
 const Render = ()=>{
     const[data,setData]  = useState(false);
 
-    if(data){
-        return(
-            <>
-            <h1>Hello This is shubham</h1>
-            <button onClick={()=>setData(false)}>Click me</button>
-            </>
-        )
-    }
-    else{
-        return(
-            <>
-            <h1>This is unknown</h1>
-            <button onClick={()=>setData(true)}>Click me</button>
+    // if(data){
+    //     return(
+    //         <>
+    //         <h1>Hello This is shubham</h1>
+    //         <button onClick={()=>setData(false)}>Click me</button>
+    //         </>
+    //     )
+    // }
+    // else{
+    //     return(
+    //         <>
+    //         <h1>This is unknown</h1>
+    //         <button onClick={()=>setData(true)}>Click me</button>
             
-            </>
-        )
-    }
+    //         </>
+    //     )
+    // }
+
+
+    return data? "Hello world" : "new World"
     
 } 
 
